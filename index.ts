c8dcbf1eabb7cefd49704d9f72caa9ed67042d20
hassan3030 <hassanhh1996eng@gmail.com>
2024-03-26 11:30:55 +0200

    goood

---
+++ index.ts
@@ -0,0 +1,7 @@
+// function show(){
+//             var i = 0 ;
+//           var confirmation = confirm("show another");
+//           if(confirmation == true ){confirm(`show another ${i++}`);}
+//         }
+//         show();
+console.log("hi");
